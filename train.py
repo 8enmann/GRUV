9e23c286dc01c0cde44d26c2808a4e98e134a503
Ben Mann <8enmann@gmail.com>
2016-10-17 15:38:13 -0700

    Initial commit Add checkpointer, tensorboard, reduce model size, add summary, add crossvalidation, use Adam instead of rmsprop, add argparse, increase batch size

--- train.py
+++ train.py
@@ -1,51 +1,71 @@
 from __future__ import absolute_import
 from __future__ import print_function
-import numpy as np
-import os
-import nn_utils.network_utils as network_utils
+from keras.callbacks import ProgbarLogger, ModelCheckpoint, EarlyStopping, TensorBoard
+import argparse
 import config.nn_config as nn_config
+import nn_utils.network_utils as network_utils
+import numpy as np
+import time
+import os
 
-config = nn_config.get_neural_net_configuration()
-inputFile = config['model_file']
-cur_iter = 0
-model_basename = config['model_basename']
-model_filename = model_basename + str(cur_iter)
+def main(model_name, num_files):
+  config = nn_config.get_neural_net_configuration()
+  inputFile = config['model_file']
+  cur_iter = 0
 
-#Load up the training data
-print ('Loading training data')
-#X_train is a tensor of size (num_train_examples, num_timesteps, num_frequency_dims)
-#y_train is a tensor of size (num_train_examples, num_timesteps, num_frequency_dims)
-X_train = np.load(inputFile + '_x.npy')
-y_train = np.load(inputFile + '_y.npy')
-print ('Finished loading training data')
 
-#Figure out how many frequencies we have in the data
-freq_space_dims = X_train.shape[2]
-hidden_dims = config['hidden_dimension_size']
+  #Load up the training data
+  print ('Loading training data')
+  #X_train is a tensor of size (num_train_examples, num_timesteps, num_frequency_dims)
+  #y_train is a tensor of size (num_train_examples, num_timesteps, num_frequency_dims)
+  X_train = np.load(inputFile + '_x.npy')
+  y_train = np.load(inputFile + '_y.npy')
+  print ('Finished loading training data')
 
-#Creates a lstm network
-model = network_utils.create_lstm_network(num_frequency_dimensions=freq_space_dims, num_hidden_dimensions=hidden_dims)
-#You could also substitute this with a RNN or GRU
-#model = network_utils.create_gru_network()
+  #Figure out how many frequencies we have in the data
+  freq_space_dims = X_train.shape[2]
+  num_timesteps = X_train.shape[1]
 
-#Load existing weights if available
-if os.path.isfile(model_filename):
-	model.load_weights(model_filename)
+  #Creates a lstm network
+  model = network_utils.create_lstm_network(num_timesteps=num_timesteps, num_frequency_dimensions=freq_space_dims)
+  #You could also substitute this with a RNN or GRU
+  #model = network_utils.create_gru_network()
 
-num_iters = 50 			#Number of iterations for training
-epochs_per_iter = 25	#Number of iterations before we save our model
-batch_size = 5			#Number of training examples pushed to the GPU per batch.
-						#Larger batch sizes require more memory, but training will be faster
-print ('Starting training!')
-while cur_iter < num_iters:
-	print('Iteration: ' + str(cur_iter))
-	#We set cross-validation to 0,
-	#as cross-validation will be on different datasets 
-	#if we reload our model between runs
-	#The moral way to handle this is to manually split 
-	#your data into two sets and run cross-validation after 
-	#you've trained the model for some number of epochs
-	history = model.fit(X_train, y_train, batch_size=batch_size, nb_epoch=epochs_per_iter, verbose=1, validation_split=0.0)
-	cur_iter += epochs_per_iter
-print ('Training complete!')
-model.save_weights(model_basename + str(cur_iter))
+  if model_name:
+    model.load_weights(model_name)
+    # TODO: verify that loaded model is compatible with new model shape
+    print ('Loaded parameters from', model_name)
+
+
+  model.summary()
+  
+  # Number of training examples pushed to the GPU per batch.
+  #Larger batch sizes require more memory, but training will be faster
+  batch_size = 8
+
+  time_str = time.strftime('%Y%m%d-%H%M')
+
+  checkpointer = ModelCheckpoint(filepath='./models/weights_%s' % time_str,
+                                 verbose=1, save_best_only=True)
+  tensorboard_reporter = TensorBoard(log_dir='./tensorboard/log_%s' % time_str,
+                                     histogram_freq=20,
+                                     write_graph=True)
+  history = model.fit(
+    X_train, y_train,
+    batch_size=batch_size,
+    nb_epoch=1000,
+    verbose=1,
+    callbacks=[checkpointer, tensorboard_reporter, ],
+    validation_split=0.05)
+
+  print ('Training complete!')
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--numfiles', default=20)
+parser.add_argument('--model_name', default=None)
+args = parser.parse_args()
+
+if __name__ == '__main__':
+  main(num_files=int(args.numfiles),
+       model_name=args.model_name,
+  )
