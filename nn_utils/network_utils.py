4fbeceb25c760697ef8be0335aab5038e32d3d74
Ben Mann <8enmann@gmail.com>
2016-10-17 19:11:21 -0700

    Forgot -a for last commit

--- nn_utils/network_utils.py
+++ nn_utils/network_utils.py
@@ -4,7 +4,7 @@ from keras.layers.wrappers import TimeDistributed
 from keras.layers.recurrent import LSTM, GRU
 from keras.optimizers import Adam
 
-def create_lstm_network(num_timesteps, num_frequency_dimensions, num_hidden_dimensions=256, num_recurrent_units=2):
+def create_lstm_network(num_timesteps, num_frequency_dimensions, num_hidden_dimensions=512, num_recurrent_units=2):
 	model = Sequential()
 	#This layer converts frequency space to hidden space
 	model.add(TimeDistributed(Dense(num_hidden_dimensions), input_shape=(num_timesteps, num_frequency_dimensions)))
